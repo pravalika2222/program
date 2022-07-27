fedc257564302eb8c86b91df0bab280de345e657
pravalika2222 <107530566+pravalika2222@users.noreply.github.com>
2022-07-27 21:33:48 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,29 @@
+# i=1
+# while i < 6:
+    # if i == 3:
+        # break
+    # print(i)
+    # i += 1
+
+# fruits=["apple","banana","cherry"]
+# for x in fruits:
+    # print(x)
+
+# def my_function():
+    # print('hello from a function')
+# my_function()
+
+# def my_function(fname,lname):
+    # print(fname)
+# my_function('python','java')
+
+# def my_function(x):
+    # return x+5
+# print(my_function(3))
+
+# def my_function(*kids):
+    # print("the youngest child is "+kids[2])
+# my_function('pr','rj','st')
+
+# def my_function(**kid):
+    # print("his last name is"+kid["lname"])
